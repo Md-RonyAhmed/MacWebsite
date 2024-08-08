ee3dfea7590f70fd772eec443b53741293fae3bb
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2024-08-08 22:44:11 +0600

    Updated files

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const mobileMenu = document.querySelector("#mobileMenu");
 const mainMenu = document.querySelector("#mainMenu");
 
 mobileMenu.addEventListener("click", () => {
-  mainMenu.classList.toggle("invisible");
+	mainMenu.classList.toggle("invisible");
 });
